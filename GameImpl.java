1646375f2e7b5b0d1cc70567afc1c388cd90ed36
ianbrussolo <ianbrussolo@hotmail.com>
2023-06-16 07:27:13 -0300

    modified:   GameImpl.java

--- GameImpl.java
+++ GameImpl.java
@@ -1,3 +1,36 @@
 public class GameImpl implements Game {
+
     
+
+    Color getSpotColor(Position position) {
+        return position.getColor();
+    }
+
+    Piece getPiece(Position position) {
+
+    }
+
+    Card getTableCard() {
+
+    }
+
+    Player getRedPlayer() {
+
+    }
+
+    Player getBluePlayer() {
+
+    }
+
+    void makeMove(Piece piece, Card card, Position position) throws IncorrectTurnOrderException, IllegalMovementException, InvalidCardException, InvalidPieceException {
+
+    }
+
+    boolean checkVictory(Color color) {
+
+    }
+
+    void printBoard() {
+
+    }
 }
